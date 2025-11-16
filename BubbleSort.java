5ceed3f7db43f0d5d4d5b76beeacd39a2524e0e5
rasangamadhushan <rmsrasanga@gmail.com>
2025-11-16 17:34:59 +0530

    run bubblesort
    Deleted the compiled code, re-entered the code, recompiled, run the code.

--- BubbleSort.java
+++ BubbleSort.java
@@ -27,13 +27,12 @@ public class BubbleSort {
         boolean swapped;
         for (int i = 0; i < n - 1; i++) {
             swapped = false;
-            for (int j = 0; j < n - i; j++){
+            for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swapped = true;
-
                 }
             }
             if (!swapped) break;
